cbae410aee28b666c1e30302393f80477d8f463c
Alexandre BROCARD <ab.nightowl33@gmail.com>
2017-08-07 10:22:04 +0200

    new maven project

---
+++ sirh-saisie-coordonnees-bancaires/src/main/java/dev/sirh_saisie_coordonnees_bancaires/App.java
@@ -0,0 +1,13 @@
+package dev.sirh_saisie_coordonnees_bancaires;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
